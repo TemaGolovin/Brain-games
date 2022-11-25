0e6d44f46e5972b36ef32cae7fb86be58cc1046a
Artem Golovin <golart1996@mail.ru>
2022-11-25 23:25:01 +0600

    update cli.js

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,7 @@ import readlineSync from 'readline-sync/lib/readline-sync.js';
 const yourName = () => {
   const gameName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${gameName}!`);
+  return gameName;
 };
 
 export default yourName;
