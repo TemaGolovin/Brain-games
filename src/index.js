073850602521d59b9c22006efcb47119a51c1fc1
TemaGolovin <golart1996@mail.ru>
2022-11-30 10:58:32 +0700

    mv and update index.js

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import readlineSync from "readline-sync/lib/readline-sync.js";
+
+export const gameLogic = (gameQuestionAnswer, task) => {
+  console.log("Welcome to the Brain Games!");
+  const gameName = readlineSync.question("May I have your name? ");
+  console.log(`Hello, ${gameName}!`);
+  console.log(`${task}`);
+  for (let i = 0; i <= 2; i += 1) {
+    const [questionForGame, answer] = gameQuestionAnswer();
+    console.log(`Question: ${questionForGame}`);
+    const answerForGame = readlineSync.question("Your answer: ");
+    if (answer === answerForGame) {
+      console.log("Correct!");
+      if (i === 2) {
+        console.log(`Congratulations, ${gameName}`);
+      }
+    } else {
+      console.log(
+        `'${answerForGame}' is wrong answer ;(. Correct answer was '${answer}'. Let's try again, ${gameName}!`
+      );
+      break;
+    }
+  }
+};
