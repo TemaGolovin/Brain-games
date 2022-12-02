c3e239f6e9232df398ac3daab951d02485a3faec
TemaGolovin <golart1996@mail.ru>
2022-12-02 11:56:14 +0700

    add brain-prime

---
+++ src/games/game-prime.js
@@ -0,0 +1,25 @@
+import gameLogic from '../index.js';
+import randNum from '../randNum.js';
+
+const minValueRange = 1;
+const maxValueRange = 50;
+
+const gameQuestionAnswer = () => {
+  const randNumber = randNum(minValueRange, maxValueRange);
+  let answer = '';
+  for (let i = 2; i <= randNumber / 2; i += 1) {
+    if (randNumber % i === 0) {
+      answer = 'no';
+      break;
+    } else {
+      answer = 'yes';
+    }
+  }
+  return [randNumber, answer];
+};
+
+const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const brainPrime = () => gameLogic(gameQuestionAnswer, task);
+
+export default brainPrime;
