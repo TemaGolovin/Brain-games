aa54cb11aef6ece1b1a41f982112b187013d3d91
TemaGolovin <golart1996@mail.ru>
2022-12-06 19:29:10 +0700

    fix code

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -1,25 +1,30 @@
 import gameLogic from '../index.js';
-import randNum from '../randNum.js';
+import randNum from '../helper.js';
 
-const gameQuestionAnswer = () => {
-  const progressionStep = randNum(1, 10); // progression step is generated in the range from 1 to 10
-  const firstNumProgression = randNum(1, 20);
-  const hiddenNumIndex = randNum(0, 9);
-  const lengthProgression = 10;
+const description = 'What number is missing in the progression?';
+
+const getProgression = (firstNum, progressionStep, length) => {
   const progression = [];
-  let nextNum = firstNumProgression;
-  for (let i = 0; i < lengthProgression; i += 1) {
+  let nextNum = firstNum;
+  for (let i = 0; i < length; i += 1) {
     progression.push(nextNum);
     nextNum += progressionStep;
   }
+  return progression;
+};
+
+const gameQuestionAnswer = () => {
+  const step = randNum(1, 10); // progression step is generated in the range from 1 to 10
+  const firstNumProgression = randNum(1, 20);
+  const hiddenNumIndex = randNum(0, 9);
+  const length = 10;
+  const progression = getProgression(firstNumProgression, step, length);
   const hiddenNum = progression[hiddenNumIndex];
   progression[hiddenNumIndex] = '..';
   const question = progression.join(' ');
   return [question, String(hiddenNum)];
 };
 
-const task = 'What number is missing in the progression?';
-
-const brainProgression = () => gameLogic(gameQuestionAnswer, task);
+const brainProgression = () => gameLogic(gameQuestionAnswer, description);
 
 export default brainProgression;
