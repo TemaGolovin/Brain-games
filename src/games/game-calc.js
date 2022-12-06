b3a64557079a28ff9bead3adbe0f7220fa747da0
TemaGolovin <golart1996@mail.ru>
2022-12-06 19:18:35 +0700

    fix

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -1,36 +1,36 @@
 import gameLogic from '../index.js';
-import randNum from '../randNum.js';
+import randNum from '../helper.js';
 
-const operators = ['+', '-', '*'];
-const minValueIndex = 0;
-const maxValueIndex = operators.length - 1;
-const minValueRange = 1;
-const maxValueRange = 50;
+const description = 'What is the result of the expression?';
 
-const gameQuestionAnswer = () => {
-  const randNumber1 = randNum(minValueRange, maxValueRange);
-  const randNumber2 = randNum(minValueRange, maxValueRange);
-  const randOperator = operators[randNum(minValueIndex, maxValueIndex)];
-  const question = `${randNumber1} ${randOperator} ${randNumber2}`;
+const colculate = (operator, operandOne, operandTwo) => {
   let resultCalc = 0;
-  switch (randOperator) {
+  switch (operator) {
     case '+':
-      resultCalc = randNumber1 + randNumber2;
+      resultCalc = operandOne + operandTwo;
       break;
     case '-':
-      resultCalc = randNumber1 - randNumber2;
+      resultCalc = operandOne - operandTwo;
       break;
     case '*':
-      resultCalc = randNumber1 * randNumber2;
+      resultCalc = operandOne * operandTwo;
       break;
     default:
       break;
   }
+  return resultCalc;
+};
+
+const gameQuestionAnswer = () => {
+  const operators = ['+', '-', '*'];
+  const randNumber1 = randNum(1, 50);
+  const randNumber2 = randNum(1, 50);
+  const randOperator = operators[randNum(0, operators.length - 1)];
+  const question = `${randNumber1} ${randOperator} ${randNumber2}`;
+  const resultCalc = colculate(randOperator, randNumber1, randNumber2);
   return [question, String(resultCalc)];
 };
 
-const task = 'What is the result of the expression?';
-
-const brainCalc = () => gameLogic(gameQuestionAnswer, task);
+const brainCalc = () => gameLogic(gameQuestionAnswer, description);
 
 export default brainCalc;
