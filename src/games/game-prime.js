a4d0cd839eeda95e4e48d244da3cbe539fadaa74
TemaGolovin <golart1996@mail.ru>
2022-12-06 19:43:59 +0700

    fix code

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -4,8 +4,8 @@ import randNum from '../helper.js';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrimeNum = (number) => {
-  if (number < 0) {
-    return false;
+  if (number <= 2) {
+    return true;
   }
 
   for (let i = 2; i <= number / 2; i += 1) {
